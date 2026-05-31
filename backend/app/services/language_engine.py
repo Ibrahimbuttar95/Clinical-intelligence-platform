1c1d8fa371f12a7f893b43da00ba5ed1e0b8a1bc
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-05-31 12:33:41 +0500

    Update language_engine.py

--- backend/app/services/language_engine.py
+++ backend/app/services/language_engine.py
@@ -1,1 +1,9 @@
+def detect_language(text: str):
 
+    urdu_chars = "اآبپتٹثجچحخدڈذرڑزژسشصضطظعغفقکگلمنوہی"
+
+    for char in text:
+        if char in urdu_chars:
+            return "urdu"
+
+    return "english"
