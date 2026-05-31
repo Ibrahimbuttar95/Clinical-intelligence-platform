7d71e82208ab3591d4c70cdf051d8d929327021e
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-05-31 17:28:26 +0500

    Update triage_repository.py

--- backend/app/respositories/triage_repository.py
+++ backend/app/respositories/triage_repository.py
@@ -1,1 +1,23 @@
+from sqlalchemy.orm import Session
 
+from app.models.triage_log import TriageLog
+
+class TriageRepository:
+
+    @staticmethod
+    def save(
+        db: Session,
+        user_id: int,
+        symptoms: str,
+        result: str
+    ):
+        log = TriageLog(
+            user_id=user_id,
+            symptoms=symptoms,
+            result=result
+        )
+
+        db.add(log)
+        db.commit()
+
+        return log
