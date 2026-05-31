3f44875d3048d6e824bafba5934641fb7aa6462e
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-05-31 12:15:46 +0500

    Update user_profile.py

--- backend/app/models/user_profile.py
+++ backend/app/models/user_profile.py
@@ -1,1 +1,24 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
 
+from app.db.base import Base
+
+class UserProfile(Base):
+
+    __tablename__ = "user_profiles"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    user_id = Column(Integer)
+
+    age = Column(Integer)
+
+    gender = Column(String)
+
+    medications = Column(String)
+
+    allergies = Column(String)
