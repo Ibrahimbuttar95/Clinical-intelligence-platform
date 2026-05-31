d643e38b763862533f3b5055c1e500af74b9a5ea
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-05-31 17:27:27 +0500

    Update subscription_repository.py

--- backend/app/respositories/subscription_repository.py
+++ backend/app/respositories/subscription_repository.py
@@ -1,1 +1,23 @@
+from sqlalchemy.orm import Session
 
+from app.models.subscription import Subscription
+
+class SubscriptionRepository:
+
+    @staticmethod
+    def create(
+        db: Session,
+        user_id: int,
+        plan: str
+    ):
+        sub = Subscription(
+            user_id=user_id,
+            plan=plan,
+            status="active"
+        )
+
+        db.add(sub)
+        db.commit()
+        db.refresh(sub)
+
+        return sub
