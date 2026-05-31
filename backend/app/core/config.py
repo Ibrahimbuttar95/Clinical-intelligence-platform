9d20a82fa07d386d1819c612ef001d3d0471ee51
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-05-31 06:22:18 +0500

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,15 @@
+import os
 
+class Settings:
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret")
+    ALGORITHM = "HS256"
+
+    DATABASE_URL = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///./local.db"
+    )
+
+    RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID", "")
+    RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET", "")
+
+settings = Settings()
