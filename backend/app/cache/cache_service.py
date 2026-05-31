81dfe7c3568871197ce9ddb0872589ac6ad1490d
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-05-31 17:32:26 +0500

    Update cache_service.py

--- backend/app/cache/cache_service.py
+++ backend/app/cache/cache_service.py
@@ -1,1 +1,15 @@
+from app.cache.redis_client import redis_client
 
+def set_cache(
+    key,
+    value,
+    ttl=300
+):
+    redis_client.setex(
+        key,
+        ttl,
+        value
+    )
+
+def get_cache(key):
+    return redis_client.get(key)
