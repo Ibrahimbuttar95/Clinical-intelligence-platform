676c07b84ed66e9957a20bf8872a59db454865f6
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-05-31 12:34:42 +0500

    Update nlp_extractor.py

--- backend/app/services/nlp_extractor.py
+++ backend/app/services/nlp_extractor.py
@@ -1,1 +1,21 @@
+def extract_symptoms(text: str):
 
+    known = [
+        "fever",
+        "headache",
+        "cough",
+        "fatigue",
+        "abdominal pain",
+        "chest pain",
+        "breathing difficulty"
+    ]
+
+    found = []
+
+    text = text.lower()
+
+    for symptom in known:
+        if symptom in text:
+            found.append(symptom)
+
+    return found
