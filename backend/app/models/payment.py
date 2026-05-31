027d6889e86340783c329d9aba05fb7505a96527
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-05-31 12:17:13 +0500

    Update payment.py

--- backend/app/models/payment.py
+++ backend/app/models/payment.py
@@ -1,1 +1,20 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
 
+from app.db.base import Base
+
+class Payment(Base):
+
+    __tablename__ = "payments"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    user_id = Column(Integer)
+
+    amount = Column(Integer)
+
+    status = Column(String)
