69b089e8c8dbbe968bb07957a9cfeca8d5050d9d
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-01 02:40:15 +0500

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
 
+void main() {
+  runApp(const ClinicalApp());
+}
+
+class ClinicalApp extends StatelessWidget {
+  const ClinicalApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Clinical Intelligence',
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
