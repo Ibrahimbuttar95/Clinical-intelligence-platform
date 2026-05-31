aec84987e995ddbf9ee55a185eee49eee61362db
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-05-31 17:29:39 +0500

    Update payment_repository.py

--- backend/app/respositories/payment_repository.py
+++ backend/app/respositories/payment_repository.py
@@ -1,1 +1,24 @@
+from sqlalchemy.orm import Session
 
+from app.models.payment import Payment
+
+class PaymentRepository:
+
+    @staticmethod
+    def create(
+        db: Session,
+        user_id: int,
+        amount: int,
+        status: str
+    ):
+
+        payment = Payment(
+            user_id=user_id,
+            amount=amount,
+            status=status
+        )
+
+        db.add(payment)
+        db.commit()
+
+        return payment
