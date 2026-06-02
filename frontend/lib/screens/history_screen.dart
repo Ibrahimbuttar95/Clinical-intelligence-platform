40bf1679270ea53e6314df14a45d21f4f0f0b665
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-02 12:07:44 +0500

    Update history_screen.dart

--- frontend/lib/screens/history_screen.dart
+++ frontend/lib/screens/history_screen.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
 
+class HistoryScreen extends StatelessWidget {
+  const HistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: const Text(
+          "Assessment History",
+        ),
+      ),
+
+      body: ListView.builder(
+
+        itemCount: 10,
+
+        itemBuilder:
+            (context, index) {
+
+          return ListTile(
+
+            leading: const Icon(
+              Icons.description,
+            ),
+
+            title: Text(
+              "Assessment ${index + 1}",
+            ),
+
+            subtitle: const Text(
+              "Previous result",
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
