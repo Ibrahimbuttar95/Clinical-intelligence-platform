baa2ea095cbbe597fc3748a1c29eddd1cb33215e
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-02 11:56:44 +0500

    Update login_screen.dart

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -1,29 +1,53 @@
 import 'package:flutter/material.dart';
 
+import 'register_screen.dart';
+import 'home_screen.dart';
+
 class LoginScreen
-    extends StatelessWidget {
+    extends StatefulWidget {
 
   const LoginScreen({
-    super.key
+    super.key,
   });
+
+  @override
+  State<LoginScreen>
+      createState() =>
+          _LoginScreenState();
+}
+
+class _LoginScreenState
+    extends State<LoginScreen> {
+
+  final emailController =
+      TextEditingController();
+
+  final passwordController =
+      TextEditingController();
+
+  bool loading = false;
 
   @override
   Widget build(
       BuildContext context) {
 
     return Scaffold(
+
       appBar: AppBar(
-        title: const Text(
-          "Login",
-        ),
+        title:
+            const Text("Login"),
       ),
+
       body: Padding(
         padding:
-            const EdgeInsets.all(16),
+            const EdgeInsets.all(20),
+
         child: Column(
           children: [
 
             TextField(
+              controller:
+                  emailController,
               decoration:
                   const InputDecoration(
                 labelText:
@@ -32,9 +56,70 @@ class LoginScreen
             ),
 
             const SizedBox(
-              height: 12,
+              height: 15,
             ),
 
+            TextField(
+              controller:
+                  passwordController,
+              obscureText: true,
+              decoration:
+                  const InputDecoration(
+                labelText:
+                    "Password",
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            SizedBox(
+              width: double.infinity,
+
+              child:
+                  ElevatedButton(
+                onPressed: () {
+
+                  Navigator.pushReplacement(
+                    context,
+
+                    MaterialPageRoute(
+                      builder: (_) =>
+                          const HomeScreen(),
+                    ),
+                  );
+                },
+
+                child:
+                    const Text(
+                        "Login"),
+              ),
+            ),
+
+            TextButton(
+              onPressed: () {
+
+                Navigator.push(
+                  context,
+
+                  MaterialPageRoute(
+                    builder: (_) =>
+                        const RegisterScreen(),
+                  ),
+                );
+              },
+
+              child: const Text(
+                "Create Account",
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
             TextField(
               obscureText: true,
               decoration:
