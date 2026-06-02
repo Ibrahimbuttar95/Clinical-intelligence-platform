848aab020116db023c3132f4d95d1a245aa7547a
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-02 12:19:54 +0500

    Update subscription_model.dart

--- frontend/lib/models/subscription_model.dart
+++ frontend/lib/models/subscription_model.dart
@@ -1,1 +1,20 @@
+class SubscriptionModel {
 
+  final String plan;
+
+  final int amount;
+
+  SubscriptionModel({
+    required this.plan,
+    required this.amount,
+  });
+
+  factory SubscriptionModel.fromJson(
+      Map<String, dynamic> json) {
+
+    return SubscriptionModel(
+      plan: json["plan"] ?? "",
+      amount: json["amount"] ?? 0,
+    );
+  }
+}
