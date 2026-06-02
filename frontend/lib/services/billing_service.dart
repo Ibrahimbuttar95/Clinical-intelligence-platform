c95e283c43149d9d7cbbf0c9ed36fbbc50149493
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-02 12:13:03 +0500

    Add BillingService with createPlan method

--- frontend/lib/services/billing_service.dart
+++ frontend/lib/services/billing_service.dart
@@ -1,1 +1,19 @@
+import '../core/api_client.dart';
+import '../core/constants.dart';
 
+class BillingService {
+
+  final ApiClient _client =
+      ApiClient();
+
+  Future<dynamic> createPlan(
+      String plan) async {
+
+    return await _client.post(
+      "${AppConstants.apiBaseUrl}/api/billing/create",
+      {
+        "plan": plan,
+      },
+    );
+  }
+}
