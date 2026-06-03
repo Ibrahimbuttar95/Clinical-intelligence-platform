bd7dbf63b49319e84f290193eb11834833a9ecdf
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-03 07:41:14 +0500

    Create api_exception.dart

---
+++ frontend/lib/exceptions/api_exception.dart
@@ -0,0 +1,14 @@
+class ApiException
+    implements Exception {
+
+  final String message;
+
+  ApiException(
+      this.message);
+
+  @override
+  String toString() {
+
+    return message;
+  }
+}
