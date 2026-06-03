9093aa921cec7a8f4ca3e1b1ac873dc06e8a4643
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-03 07:32:45 +0500

    Update risk_utils.dart

--- frontend/lib/utils/risk_utils.dart
+++ frontend/lib/utils/risk_utils.dart
@@ -1,1 +1,16 @@
+class RiskUtils {
 
+  static String classify(
+      int score) {
+
+    if (score < 30) {
+      return "Low";
+    }
+
+    if (score < 70) {
+      return "Moderate";
+    }
+
+    return "High";
+  }
+}
