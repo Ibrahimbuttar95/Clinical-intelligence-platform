e6a14562678ab1acea5d754993bcc335425d500e
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-03 07:29:26 +0500

    Add BillingRepository with createPlan method

--- frontend/lib/repositories/billing_repository.dart
+++ frontend/lib/repositories/billing_repository.dart
@@ -1,1 +1,15 @@
+import '../services/billing_service.dart';
 
+class BillingRepository {
+
+  final BillingService _service =
+      BillingService();
+
+  Future<dynamic> createPlan(
+      String plan) async {
+
+    return await _service.createPlan(
+      plan,
+    );
+  }
+}
