fc2fd04b5c317853057f940cbceaba49cd820e6f
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-03 07:29:55 +0500

    Update history_repository.dart

--- frontend/lib/repositories/history_repository.dart
+++ frontend/lib/repositories/history_repository.dart
@@ -1,1 +1,16 @@
+import '../core/api_client.dart';
+import '../core/constants.dart';
 
+class HistoryRepository {
+
+  final ApiClient _client =
+      ApiClient();
+
+  Future<dynamic> getHistory() async {
+
+    return await _client.post(
+      "${AppConstants.apiBaseUrl}/api/history",
+      {},
+    );
+  }
+}
