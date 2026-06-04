5184d5418581b6ed40e187e3264d4bcbcb93c50e
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-04 07:57:42 +0500

    Update register_screen.dart

--- frontend/lib/screens/register_screen.dart
+++ frontend/lib/screens/register_screen.dart
@@ -1,40 +1,10 @@
-import 'package:flutter/material.dart';
+final authRepo =
+    AuthRepository();
 
-class RegisterScreen
-    extends StatefulWidget {
-
-  const RegisterScreen({
-    super.key,
-  });
-
-  @override
-  State<RegisterScreen>
-      createState() =>
-          _RegisterScreenState();
-}
-
-class _RegisterScreenState
-    extends State<RegisterScreen> {
-
-  final emailController =
-      TextEditingController();
-
-  final passwordController =
-      TextEditingController();
-
-  @override
-  Widget build(
-      BuildContext context) {
-
-    return Scaffold(
-
-      appBar: AppBar(
-        title:
-            const Text(
-          "Register",
-        ),
-      ),
-
+await authRepo.register(
+  emailController.text,
+  passwordController.text,
+);
       body: Padding(
         padding:
             const EdgeInsets.all(20),
