12f0ca407fb9a0e9d8ad3ac2017514dc1f2eea91
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-18 03:48:47 +0500

    Create SettingsModel class for application settings

---
+++ frontend/lib/settings/settings_model.dart
@@ -0,0 +1,14 @@
+class SettingsModel {
+
+  bool notifications;
+
+  bool voiceEnabled;
+
+  String language;
+
+  SettingsModel({
+    required this.notifications,
+    required this.voiceEnabled,
+    required this.language,
+  });
+}
