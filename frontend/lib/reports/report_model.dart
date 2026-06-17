e81187252e61e4029c4742b16a4fc0608372b7dd
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-18 04:01:15 +0500

    Update report_model.dart

--- frontend/lib/reports/report_model.dart
+++ frontend/lib/reports/report_model.dart
@@ -1,1 +1,14 @@
+class ReportModel {
 
+  final String id;
+
+  final String date;
+
+  final String result;
+
+  ReportModel({
+    required this.id,
+    required this.date,
+    required this.result,
+  });
+}
