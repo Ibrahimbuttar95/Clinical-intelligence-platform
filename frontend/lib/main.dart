05c3c988d11dab19c151e5bc542ecdec71230484
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-18 04:33:33 +0500

    Refactor main.dart to use MultiProvider

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,31 +1,68 @@
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'providers/auth_provider.dart';
 import 'providers/language_provider.dart';
 import 'providers/subscription_provider.dart';
 import 'providers/triage_provider.dart';
-  runApp(
-    const ClinicalIntelligenceApp(),
-  );
+
+import 'screens/splash_screen.dart';
+
+void main() {
+runApp(
+const ClinicalIntelligenceApp(),
+);
 }
 
 class ClinicalIntelligenceApp extends StatelessWidget {
-  const ClinicalIntelligenceApp({
-    super.key,
-  });
+const ClinicalIntelligenceApp({
+super.key,
+});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Clinical Intelligence Platform',
+@override
+Widget build(BuildContext context) {
 
-      debugShowCheckedModeBanner: false,
+return MultiProvider(
 
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
+  providers: [
 
-      home: const SplashScreen(),
-    );
-  }
+    ChangeNotifierProvider(
+      create: (_) =>
+          AuthProvider(),
+    ),
+
+    ChangeNotifierProvider(
+      create: (_) =>
+          LanguageProvider(),
+    ),
+
+    ChangeNotifierProvider(
+      create: (_) =>
+          SubscriptionProvider(),
+    ),
+
+    ChangeNotifierProvider(
+      create: (_) =>
+          TriageProvider(),
+    ),
+  ],
+
+  child: MaterialApp(
+
+    title:
+        'Clinical Intelligence Platform',
+
+    debugShowCheckedModeBanner:
+        false,
+
+    theme: ThemeData(
+      useMaterial3: true,
+    ),
+
+    home:
+        const SplashScreen(),
+  ),
+);
+
+}
 }
