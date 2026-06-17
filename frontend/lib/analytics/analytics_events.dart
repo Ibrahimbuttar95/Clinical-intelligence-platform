49a5feb9f185f8ddcfee60eab3c588f2a134f079
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-18 04:05:03 +0500

    Add AnalyticsEvents class with event constants

---
+++ frontend/lib/analytics/analytics_events.dart
@@ -0,0 +1,14 @@
+class AnalyticsEvents {
+
+  static const login =
+      "login";
+
+  static const register =
+      "register";
+
+  static const assessment =
+      "assessment";
+
+  static const subscription =
+      "subscription";
+}
