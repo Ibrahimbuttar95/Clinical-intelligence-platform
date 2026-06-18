58ff74237ef987e7942f6480df8c7f3293ae4cc5
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-18 11:50:01 +0500

    Update subscription_response.dart

--- frontend/lib/models/api/subscription_response.dart
+++ frontend/lib/models/api/subscription_response.dart
@@ -1,1 +1,30 @@
+class SubscriptionResponse {
 
+final String plan;
+
+final bool active;
+
+final String expiry;
+
+SubscriptionResponse({
+required this.plan,
+required this.active,
+required this.expiry,
+});
+
+factory SubscriptionResponse.fromJson(
+Map<String, dynamic> json) {
+
+return SubscriptionResponse(
+  plan:
+      json["plan"] ?? "free",
+
+  active:
+      json["active"] ?? false,
+
+  expiry:
+      json["expiry"] ?? "",
+);
+
+}
+}
