c44f9ecd1ce614356878f054bb892b79936e309e
Ibrahimbuttar95 <ibrahimahmadbuttar95@gmail.com>
2026-06-18 11:51:05 +0500

    Update app_router.dart

--- frontend/lib/navigation/app_router.dart
+++ frontend/lib/navigation/app_router.dart
@@ -1,1 +1,82 @@
+import 'package:flutter/material.dart';
 
+import '../screens/home_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/register_screen.dart';
+import '../screens/profile_screen.dart';
+import '../screens/history_screen.dart';
+import '../screens/triage_screen.dart';
+import '../screens/subscription_screen.dart';
+import '../screens/voice_screen.dart';
+import '../screens/settings_screen.dart';
+
+class AppRouter {
+
+static Route<dynamic> generate(
+RouteSettings settings) {
+
+switch (settings.name) {
+
+  case "/login":
+    return MaterialPageRoute(
+      builder: (_) =>
+          const LoginScreen(),
+    );
+
+  case "/register":
+    return MaterialPageRoute(
+      builder: (_) =>
+          const RegisterScreen(),
+    );
+
+  case "/home":
+    return MaterialPageRoute(
+      builder: (_) =>
+          const HomeScreen(),
+    );
+
+  case "/profile":
+    return MaterialPageRoute(
+      builder: (_) =>
+          const ProfileScreen(),
+    );
+
+  case "/history":
+    return MaterialPageRoute(
+      builder: (_) =>
+          const HistoryScreen(),
+    );
+
+  case "/triage":
+    return MaterialPageRoute(
+      builder: (_) =>
+          const TriageScreen(),
+    );
+
+  case "/subscription":
+    return MaterialPageRoute(
+      builder: (_) =>
+          const SubscriptionScreen(),
+    );
+
+  case "/voice":
+    return MaterialPageRoute(
+      builder: (_) =>
+          const VoiceScreen(),
+    );
+
+  case "/settings":
+    return MaterialPageRoute(
+      builder: (_) =>
+          const SettingsScreen(),
+    );
+
+  default:
+    return MaterialPageRoute(
+      builder: (_) =>
+          const LoginScreen(),
+    );
+}
+
+}
+}
